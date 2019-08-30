f76c2986e3d37ccef83bb2a1aa981b63b22fb29a
Raphael França <raphaelfrancabsb@gmail.com>
2019-08-30 11:37:54 -0300

    add services and repositories

--- DemoStore.Domain/StoreContext/Enums/EAddressType.cs
+++
@@ -1,9 +0,0 @@
-
-namespace DemoStore.Domain.StoreContext.Enums
-{
-    public enum EAddressType
-    {
-        Shipping = 1,
-        Billing = 2
-    }
-}
--- DemoStore.Shared/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DemoStore.Shared
-{
-    public class Class1
-    {
-    }
-}
